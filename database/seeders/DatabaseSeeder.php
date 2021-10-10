f55714fe1180707e3f4e65d95a5129ad0acca20e
sohailtahir5858 <sohailtahir5858@gmail.com>
2021-10-10 20:04:06 +0500

    implementing the seeder and faker option

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,33 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\STR;
+use Faker\Factory as Faker;
+
+
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Faker::create();
+        foreach (range(1, 50) as $list) {
+            DB::table('customers')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'mobile' => $faker->numerify('##########'),
+                'password' => $faker->password,
+                'address' => $faker->address,
+                'city' => $faker->city,
+                'state' => STR::random(10),
+                'company' => $faker->company,
+                'zip' => $faker->numerify('######'),
+                'status' => $faker->numberBetween(0, 1),
+                'created_at' => $faker->date($format = 'Y-m-d', $max = 'now'),
+
+            ]);
+        }
     }
 }
